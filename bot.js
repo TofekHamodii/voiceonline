da71174dda6249179e2abf28ba6adeef87affe75
Simple <43014320+TofekHamodii@users.noreply.github.com>
2018-10-31 12:54:38 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,4 +9,26 @@ client.on('voiceStateUpdate', (old, now) => {
   if (currentSize !== size) channel.setName(`Ritual Online : ${currentSize} `);
 });
 
+var prefix = "^"
+client.on("message", async message => {
+    if(message.content.startsWith(prefix + "join")) {
+        if(message.author.id === "452379478618800129") {
+        
+      let args = message.content.split(" ").slice(1).join(" ");
+        if(!args) {
+          return message.channel.send("**يرجــى تحديـد روم صوتــي مـع الآمـر ء .**");
+        }
+          let room = message.guild.channels.find(a => a.name.includes(args));
+            room.join() .then(() => {
+              message.channel.send(`**تـم تثبيــت البـوت فـي روم , \`${room.name}\`**`) .then((m) => {
+                m.delete(5000);
+              });
+            });
+              
+    } else {
+      return message.channel.send("**آنــت لست مؤهــل لآستخـدآم هذا الآمــر ء .**");
+    }
+  }
+  });
+
 client.login(process.env.BOT_TOKEN);
