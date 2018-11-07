9dc30530d1c0e4560d91f1dd02903829630a8838
Simple <43014320+TofekHamodii@users.noreply.github.com>
2018-11-07 22:36:42 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,17 +2,17 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('voiceStateUpdate', (old, now) => {
-  const channel = client.channels.get('506539714258534420');
+  const channel = client.channels.get('509827374561624080');
   const currentSize = channel.guild.members.filter(m => m.voiceChannel).size;
   const size = channel.name.match(/\[\s(\d+)\s\]/);
   if (!size) return channel.setName(`Ritual Online : ${currentSize} `);
-  if (currentSize !== size) channel.setName(`Ritual Online : ${currentSize} `);
+  if (currentSize !== size) channel.setName(`Universe Online : ${currentSize} `);
 });
 
 var prefix = "^"
 client.on("message", async message => {
     if(message.content.startsWith(prefix + "join")) {
-        if(message.author.id === "452379478618800129") {
+        if(message.author.id === "509827374561624080") {
         
       let args = message.content.split(" ").slice(1).join(" ");
         if(!args) {
