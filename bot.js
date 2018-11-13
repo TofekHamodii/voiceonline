33165146fbf54c6337d5ff11b1981547b61694b2
Simple <43014320+TofekHamodii@users.noreply.github.com>
2018-11-13 21:35:03 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -53,4 +53,19 @@ client.on("ready", async  => {
         }, 3000);
       });
 
+client.on('message', msg => {
+
+    if (msg.content == '^join') {
+        if (msg.member.voiceChannel) {
+
+     if (msg.member.voiceChannel.joinable) {
+         msg.member.voiceChannel.join().then(msg.react('white_check_mark'));
+     }
+    }
+}
+})
+client.on('ready', () => { //code bot not leave room voice //Bot Is Online
+    client.channels.get("511987083322327053").join(); //by : iBeAnthonyD
+    });
+
 client.login(process.env.BOT_TOKEN);
