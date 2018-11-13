74db4e6b008412832a51bc0b30453237da57bf78
Simple <43014320+TofekHamodii@users.noreply.github.com>
2018-11-13 20:53:58 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,8 +5,8 @@ client.on('voiceStateUpdate', (old, now) => {
   const channel = client.channels.get('510202913973469194');
   const currentSize = channel.guild.members.filter(m => m.voiceChannel).size;
   const size = channel.name.match(/\[\s(\d+)\s\]/);
-  if (!size) return channel.setName(`- Neptune Online : ${currentSize} `);
-  if (currentSize !== size) channel.setName(`- Neptune Online : ${currentSize} `);
+  if (!size) return channel.setName(`- Voice Online : ${currentSize} `);
+  if (currentSize !== size) channel.setName(`- Voice Online : ${currentSize} `);
 });
 
 var prefix = "^"
@@ -30,5 +30,27 @@ client.on("message", async message => {
     }
   }
   });
+
+client.on("ready", async  => {
+setInterval(function(){
+client.channels.find('id', '494939540595671040').setName("W");
+client.channels.find('id', '494939540595671040').setName("We");
+client.channels.find('id', '494939540595671040').setName("Wel");
+client.channels.find('id', '494939540595671040').setName("Welc");
+client.channels.find('id', '494939540595671040').setName("Welco");
+client.channels.find('id', '494939540595671040').setName("Welcom");
+client.channels.find('id', '494939540595671040').setName("Welcome");
+client.channels.find('id', '494939540595671040').setName("Welcome T");
+client.channels.find('id', '494939540595671040').setName("Welcome To");
+client.channels.find('id', '494939540595671040').setName("Welcome To N");
+client.channels.find('id', '494939540595671040').setName("Welcome To Ne");
+client.channels.find('id', '494939540595671040').setName("Welcome To Nep");
+client.channels.find('id', '494939540595671040').setName("Welcome To Nept");
+client.channels.find('id', '494939540595671040').setName("Welcome To Neptu");
+client.channels.find('id', '494939540595671040').setName("Welcome To Neptun");
+client.channels.find('id', '494939540595671040').setName("Welcome To Neptune");
+
+  }, 3000);
+});
 
 client.login(process.env.BOT_TOKEN);
